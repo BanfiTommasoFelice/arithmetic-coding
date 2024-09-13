f7e2a97db54c1a194266607146fbcbf26261fb96
Gabr1313 <gabriele.baietta03@gmail.com>
2024-09-13 09:45:13 +0200

    now main is more readable

--- src/main.c
+++ src/main.c
@@ -18,26 +18,21 @@ i32 main(void) {
     u8Vec          data      = string_to_u8vec(string_read_file(stdin));
     u32Vec const   cum_distr = cum_distr_from_rnd_u8vec(data);
     struct timeval end, start;
-
-    fprintf(stdout, "Data         : %6lu bytes\n", data.len);
-#ifdef PRINT_CONTENT
-    fprintf(stdout, "`%s`\n", data.ptr);
-#endif
-
     time_of(Message encoded = arithmetic_encoder(data, cum_distr), f64 encoding_time);
-
-    fprintf(stdout, "Encoded      : %6lu bytes\n", encoded.len);
-#ifdef PRINT_CONTENT
-    fprintf(stdout, "`");
-    message_print_hex(stdout, encoded);
-    fprintf(stdout, "`\n");
-#endif
-
     time_of(u8Vec decoded = arithmetic_decoder(&encoded, cum_distr), f64 decoding_time);
 
-#ifdef PRINT_CONTENT
-    fprintf(stdout, "Decoded      : %6lu bytes\n", decoded.len);
-    fprintf(stdout, "`%s`\n", decoded.ptr);
+#ifndef PRINT_CONTENT
+    printf("Data         : %6lu bytes\n", data.len);
+    printf("Encoded      : %6lu bytes\n", encoded.len);
+#else
+    printf("Data         : %6lu bytes\n", data.len);
+    printf("`%s`\n", data.ptr);
+    printf("Encoded      : %6lu bytes\n", encoded.len);
+    printf("`");
+    message_print_hex(stdout, encoded);
+    printf("`\n");
+    printf("Decoded      : %6lu bytes\n", decoded.len);
+    printf("`%s`\n", decoded.ptr);
 #endif
 
     fflush(stdout);
@@ -45,9 +40,9 @@ i32 main(void) {
     for (u32 i = 0; i < data.len; i++)
         assert(data.ptr[i] == decoded.ptr[i] && "original data and decoded data are different");
 
-    fprintf(stdout, "Compression  : %6.2f %%\n", (double)encoded.len * 100 / data.len);
-    fprintf(stdout, "Encoding time: %6.3f seconds \n", encoding_time);
-    fprintf(stdout, "Decoding time: %6.3f seconds \n", decoding_time);
+    printf("Compression  : %6.2f %%\n", (double)encoded.len * 100 / data.len);
+    printf("Encoding time: %6.3f seconds \n", encoding_time);
+    printf("Decoding time: %6.3f seconds \n", decoding_time);
 
     u8vec_free(data);
     u32vec_free(cum_distr);
